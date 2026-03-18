ffea2848704cb58a009bf5b2dd1c14576b9a22b0
Werner Donné <werner.donne@pincette.biz>
2026-03-18 16:26:24 +0100

    Update dependencies

--- src/main/java/net/pincette/letterbox/Common.java
+++ src/main/java/net/pincette/letterbox/Common.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 class Common {
   static final Logger LOGGER = getLogger("net.pincette.letterbox");
   static final String LETTER_BOX = "letter-box";
-  static final String VERSION = "1.3.5";
+  static final String VERSION = "1.3.6";
 
   private Common() {}
 }
